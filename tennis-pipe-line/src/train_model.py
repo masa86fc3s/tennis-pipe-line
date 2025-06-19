a022238310db07bcee7c20a59699df6d7cb0752a
masa86fc3s <msn.masamasa.fc3s@gmail.com>
2025-06-19 17:29:06 +0900

    a

--- tennis-pipe-line/src/train_model.py
+++ tennis-pipe-line/src/train_model.py
@@ -33,6 +33,7 @@ from typing import Tuple, List
 from pandas import DataFrame
 
 
+
 class LightGBMPipeline:
     def __init__(self, config_path: str = "../yml/s3_data.yml"):
         base_dir = os.path.dirname(os.path.abspath(__file__))
